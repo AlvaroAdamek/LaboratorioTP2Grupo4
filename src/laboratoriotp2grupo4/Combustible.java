ef07f98a1ce50edb613b7a391ae0ff162b9e0889
paulocanestrari <113647158+paulocanestrari@users.noreply.github.com>
2022-09-22 16:26:31 -0300

    correccion

--- src/laboratoriotp2grupo4/Combustible.java
+++ src/laboratoriotp2grupo4/Combustible.java
@@ -5,16 +5,6 @@ public class Combustible {
     private String tipo;
     private Double precio;
 
-//    public Combustible(String tipo,Double precio) {
-//        this.tipo = tipo;
-//        if (tipo.equalsIgnoreCase("Nafta")) {
-//            this.precio = 137.00;
-//        } else if (tipo.equalsIgnoreCase("Gasoil")) {
-//            this.precio = 186.00;
-//        } else {
-//            this.precio = precio;
-//        }}
-
     public Combustible(String tipo, Double precio) {
         this.tipo = tipo;
         this.precio = precio;
